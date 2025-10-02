3faa7257ed0935f9e791c342e1bdc71af1034bc1
nixezz <ban.rak228@gmail.com>
2025-10-02 10:07:55 +0300

    Инициализация Rest Assured

---
+++ src/test/java/ninja/rail/constants/ApiConfig.java
@@ -0,0 +1,34 @@
+package ninja.rail.constants;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static ninja.rail.constants.Constant.API.HOST;
+
+public class ApiConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ApiConfig.class);
+    private static RequestSpecification requestSpec;
+
+    public static void init() {
+        if (requestSpec == null) {
+            logger.info("Инициализация конфигурации RestAssured с baseURI: {}", HOST);
+            RestAssured.baseURI = HOST;
+            requestSpec = new RequestSpecBuilder()
+                    .setContentType(ContentType.JSON)
+                    .build();
+            logger.debug("RequestSpecification успешно создана");
+        }
+    }
+
+    public static RequestSpecification getRequestSpec() {
+        if (requestSpec == null) {
+            logger.warn("RequestSpecification не инициализирована, вызывается init()");
+            init();
+        }
+        return requestSpec;
+    }
+}
