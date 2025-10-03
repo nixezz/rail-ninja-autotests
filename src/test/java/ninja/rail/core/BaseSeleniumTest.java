45f5b138ebe6488eb83faaaa32b5c913f0f989ce
nixezz <ban.rak228@gmail.com>
2025-10-03 19:07:16 +0300

    Fixed launch of the web driver, minor changes in the web driver settings during UI testing

--- src/test/java/ninja/rail/core/BaseSeleniumTest.java
+++ src/test/java/ninja/rail/core/BaseSeleniumTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
@@ -16,16 +18,20 @@ import static ninja.rail.constants.Constant.TimeoutVariable.PAGE_LOAD_TIMEOUT;
 @ExtendWith(TestListener.class)
 public abstract class BaseSeleniumTest {
     protected WebDriver driver;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseSeleniumTest.class);
 
     @BeforeEach
     public void setUp() {
+        LOGGER.info("Starting setUp procedure..");
         WebDriverManager.chromedriver().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
-//        chromeOptions.addArguments("--headless", "--disable-gpu");
+        chromeOptions.addArguments("--headless", "--disable-gpu");
         driver = new ChromeDriver(chromeOptions);
+        LOGGER.info("WebDriver initialized: " + (driver != null));
         driver.manage().window().setSize(new Dimension(1920, 1080));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGE_LOAD_TIMEOUT));
+        LOGGER.info("SetUp completed.");
     }
 
 }
