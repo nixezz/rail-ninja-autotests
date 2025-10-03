b0cd184855d6cbb19668e2af7562f9376f9a463f
nixezz <ban.rak228@gmail.com>
2025-10-03 14:14:18 +0300

    Minor import changes

--- src/test/java/ninja/rail/pages/pasengers/PassengersLegacyPage.java
+++ src/test/java/ninja/rail/pages/pasengers/PassengersLegacyPage.java
@@ -1,7 +1,6 @@
 package ninja.rail.pages.pasengers;
 
 import ninja.rail.core.BaseSeleniumPage;
-import ninja.rail.pages.PassengerDetailsPage;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
