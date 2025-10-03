7db9e7a35ee31503025cbd440806321578f21b83
nixezz <ban.rak228@gmail.com>
2025-10-03 14:22:54 +0300

    Changing the monitored logic when opening the train schedule tab (TimetablePage classes). Translation of the language in the logs into English (fixing a bug with encoding output). Minor changes to the step headers for Allure

--- src/test/java/ninja/rail/pages/MainPage.java
+++ src/test/java/ninja/rail/pages/MainPage.java
@@ -2,19 +2,27 @@ package ninja.rail.pages;
 
 import io.qameta.allure.Step;
 import ninja.rail.core.BaseSeleniumPage;
+import ninja.rail.pages.timetable.TimetableLegacyPage;
+import ninja.rail.pages.timetable.TimetablePage;
+import ninja.rail.pages.timetable.TimetableV9Page;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 
+import static ninja.rail.constants.Constant.TimeoutVariable.EXPLICIT_WAIT;
+import static ninja.rail.constants.Constant.TimeoutVariable.EXPLICIT_WAIT_25;
 import static ninja.rail.constants.Constant.Urls.TIMETABLE_PAGE;
 import static ninja.rail.constants.Constant.Urls.TIMETABLE_V9_PAGE;
 
 public class MainPage extends BaseSeleniumPage {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainPage.class);
+    private final WebDriverWait wait;
 
     @FindBy(xpath = "//*[@id='departure_station']")
     private WebElement departureStationInput;
@@ -61,10 +69,12 @@ public class MainPage extends BaseSeleniumPage {
     public MainPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, EXPLICIT_WAIT);
     }
 
     @Step("Открытие главной страницы https://rail.ninja")
     public MainPage openMainPage() {
+        LOGGER.info("Opening the main page: {}", BASE_URL);
         driver.get(BASE_URL);
         PageFactory.initElements(driver, this);
         return this;
@@ -72,146 +82,125 @@ public class MainPage extends BaseSeleniumPage {
 
     @Step("Выбор пункта отправления: {stationName}")
     public MainPage enterDepartureStation(String stationName) {
-        LOGGER.info("Ввод пункта отправления: {}", stationName);
-        try{
-            departureStationInput.click();
+        LOGGER.info("Entering the departure point: {}", stationName);
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(departureStationInput)).click();
             departureStationInput.clear();
             departureStationInput.sendKeys(stationName);
             Thread.sleep(1000);
             departureStationInput.sendKeys(Keys.ENTER);
-        } catch (InterruptedException e){
-            LOGGER.error("Произошла ошибка при выборе города отправления: {}", stationName, e);
-            throw new RuntimeException("Произошла ошибка при выборе города отправления: " + stationName, e);
+            LOGGER.info("Departure point '{}' selected.", stationName);
+        } catch (Exception e) {
+            LOGGER.error("Error when selecting the departure point '{}': {}", stationName, e.getMessage());
+            throw new RuntimeException("Error when selecting the departure point: " + stationName, e);
         }
         return this;
     }
 
     @Step("Выбор пункта прибытия: {stationName}")
     public MainPage enterArrivalStation(String stationName) {
-        LOGGER.info("Ввод пункта прибытия: {}", stationName);
-        try{
-            waitForElementVisible(arrivalStationInput);
-            waitForElementClickable(arrivalStationInput);
-            arrivalStationInput.click();
+        LOGGER.info("Entering an arrival point: {}", stationName);
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(arrivalStationInput)).click();
             arrivalStationInput.clear();
             arrivalStationInput.sendKeys(stationName);
             Thread.sleep(1000);
             arrivalStationInput.sendKeys(Keys.ENTER);
-        } catch (InterruptedException e){
-            LOGGER.error("Произошла ошибка при выборе города отправления: {}", stationName, e);
-            throw new RuntimeException("Произошла ошибка при выборе города отправления: " + stationName, e);
+            LOGGER.info("Arrival point '{}' selected.", stationName);
+        } catch (Exception e) {
+            LOGGER.error("Error when selecting the arrival point '{}': {}", stationName, e.getMessage());
+            throw new RuntimeException("Error when selecting the arrival point: " + stationName, e);
         }
         return this;
     }
 
     @Step("Выбор двух взрослых пассажиров")
-    public MainPage setTwoAdultPassenger(){
-        LOGGER.info("Установка двух взрослых пассажиров.");
-        try{
-            waitForElementVisible(travelersButton);
-            waitForElementClickable(travelersButton);
-            travelersButton.click();
-            waitForElementVisible(incrementAdultPassengerCounter);
-            waitForElementClickable(incrementAdultPassengerCounter);
-            incrementAdultPassengerCounter.click();
-            Thread.sleep(500);
-            waitForElementVisible(incrementAdultPassengerCounter);
-            waitForElementClickable(incrementAdultPassengerCounter);
-            Thread.sleep(500);
-            backgroundDiv.click();
-        } catch (InterruptedException e){
-            LOGGER.error("Произошла ошибка при установлении количества пассажиров: ", e);
-            throw new RuntimeException("Произошла ошибка при установлении количества пассажиров: ", e);
+    public MainPage setTwoAdultPassenger() {
+        LOGGER.info("The choice of two adult passengers.");
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(travelersButton)).click();
+            wait.until(ExpectedConditions.elementToBeClickable(incrementAdultPassengerCounter)).click();
+            wait.until(ExpectedConditions.elementToBeClickable(incrementAdultPassengerCounter)).click();
+            wait.until(ExpectedConditions.elementToBeClickable(backgroundDiv)).click();
+            LOGGER.info("Two adult passengers selected.");
+        } catch (Exception e) {
+            LOGGER.error("Error when setting the number of passengers: {}", e.getMessage());
+            throw new RuntimeException("Error when setting the number of passengers", e);
         }
         return this;
     }
 
     @Step("Установка даты в календаре (5 ноября 2025)")
-    public MainPage setFifthOfNovemberDate(){
-        LOGGER.info("Установка даты (5 ноября 2025).");
-        try{
-            waitForElementVisible(calendarButton);
-            waitForElementClickable(calendarButton);
-            calendarButton.click();
-            waitForElementVisible(calendarNextMonthButton);
-            waitForElementClickable(calendarNextMonthButton);
-            calendarNextMonthButton.click();
-            Thread.sleep(500);
-            waitForElementVisible(dateDiv);
-            waitForElementClickable(dateDiv);
-            dateDiv.click();
-        } catch (InterruptedException e){
-            LOGGER.error("Произошла ошибка при установке даты (5 ноября 2025): ", e);
-            throw new RuntimeException("Произошла ошибка при установке даты (5 ноября 2025): ", e);
+    public MainPage setFifthOfNovemberDate() {
+        LOGGER.info("Setting the date (November 5, 2025).");
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(calendarButton)).click();
+            wait.until(ExpectedConditions.elementToBeClickable(calendarNextMonthButton)).click();
+            wait.until(ExpectedConditions.elementToBeClickable(dateDiv)).click();
+            LOGGER.info("November 5, 2025 selected.");
+        } catch (Exception e) {
+            LOGGER.error("Error when setting the date (November 5, 2025): {}", e.getMessage());
+            throw new RuntimeException("Error when setting the date (November 5, 2025)", e);
         }
         return this;
     }
 
-    @Step("Нажатие на кнопку 'Search trains'")
-    public TimetablePage searchButtonClick(){
-        LOGGER.info("Нажатие на кнопку 'Search trains'. Переход на страницу {}", TIMETABLE_V9_PAGE);
-        try {
-            waitForElementClickable(submitButton);
-            submitButton.click();
-            return switchMonitorToTimetablePage();
-        } catch (InterruptedException e){
-            LOGGER.error("При нажатии на кнопку поиска произошла ошибка: ", e);
-            throw new RuntimeException("При нажатии на кнопку поиска произошла ошибка:", e);
-        }
-    }
+    @Step("Нажатие на кнопку 'Search trains' и переключение на новую вкладку (расписания поездов)")
+    public TimetablePage searchButtonClick() {
+        LOGGER.info("Clicking on the 'Search trains' button");
+        String originalWindow = driver.getWindowHandle();
 
-    @Step("Переключение на страницу расписания поездов")
-    public TimetablePage switchMonitorToTimetablePage() {
-        LOGGER.info("Переключение монитора на странницу: {}", TIMETABLE_V9_PAGE);
         try {
-            String originalWindow = driver.getWindowHandle();
+            int initialWindowCount = driver.getWindowHandles().size();
+            LOGGER.info("Number of windows before a click: {}", initialWindowCount);
+
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(submitButton));
+            button.click();
+            LOGGER.info("The 'Search trains' button has been clicked.");
+
+            LOGGER.info("Waiting for a new tab to open...");
+            wait.until(ExpectedConditions.numberOfWindowsToBe(initialWindowCount + 1));
+
             Set<String> allWindows = driver.getWindowHandles();
-            if (allWindows.size() > 1) {
-                for (String windowHandle : allWindows) {
-                    if (!windowHandle.equals(originalWindow)) {
-                        driver.switchTo().window(windowHandle);
-                        break;
-                    }
+            String newWindowHandle = null;
+            for (String windowHandle : allWindows) {
+                if (!windowHandle.equals(originalWindow)) {
+                    newWindowHandle = windowHandle;
+                    break;
                 }
             }
-            waitForUrlContains(TIMETABLE_V9_PAGE);
-            LOGGER.info("Переключение на другую страницу произошло успешно. Текущее URL: {}", driver.getCurrentUrl());
-            return new TimetablePage(driver);
+            if (newWindowHandle == null) {
+                throw new RuntimeException("Couldn't find a new tab");
+            }
+
+            driver.switchTo().window(newWindowHandle);
+            LOGGER.info("Switching to a new tab is done. Handle: {}", newWindowHandle);
+            WebDriverWait extendedWait = new WebDriverWait(driver, EXPLICIT_WAIT_25);
+            extendedWait.until(ExpectedConditions.or(
+                    ExpectedConditions.urlContains(TIMETABLE_PAGE),
+                    ExpectedConditions.urlContains(TIMETABLE_V9_PAGE)
+            ));
+            String currentUrl = driver.getCurrentUrl();
+            LOGGER.info("Current URL of the new tab: {}", currentUrl);
+
+            if (currentUrl != null && currentUrl.contains(TIMETABLE_V9_PAGE)) {
+                LOGGER.info("Creating a TimetableV9Page object for a URL: {}", currentUrl);
+                return new TimetableV9Page(driver);
+            } else if (currentUrl != null && currentUrl.contains(TIMETABLE_PAGE)) {
+                LOGGER.info("Creating a TimetableLegacyPage object for a URL: {}", currentUrl);
+                return new TimetableLegacyPage(driver);
+            } else {
+                LOGGER.error("Unknown URL: {}", currentUrl);
+                throw new RuntimeException("Unknown URL: " + currentUrl);
+            }
+
+        } catch (TimeoutException e) {
+            LOGGER.error("Timeout while waiting for a new tab or URL to load: {}", e.getMessage());
+            LOGGER.info("Current open tabs: {}", driver.getWindowHandles().size());
+            throw new RuntimeException("Timeout when switching to a new tab", e);
         } catch (Exception e) {
-            LOGGER.error("При переключении монитора на другую страницу произошла ошибка: ", e);
-            throw new RuntimeException("При переключении монитора на другую страницу произошла ошибка: " + TIMETABLE_V9_PAGE, e);
+            LOGGER.error("Error when clicking on the 'Search trains' button or switching to a new tab: {}", e.getMessage());
+            throw new RuntimeException("Error when clicking on the 'Search trains' button or switching to a new tab", e);
         }
-    }
-
-
-
-    private void waitForElementVisible(WebElement element) throws InterruptedException {
-        for (int i = 0; i < 30; i++) { // Max 15 seconds (30 * 500ms)
-            if (element.isDisplayed()) {
-                return;
-            }
-            Thread.sleep(500);
-        }
-        throw new TimeoutException("Element not visible: " + element);
-    }
-
-    private void waitForElementClickable(WebElement element) throws InterruptedException {
-        for (int i = 0; i < 30; i++) { // Max 15 seconds (30 * 500ms)
-            if (element.isEnabled() && element.isDisplayed()) {
-                return;
-            }
-            Thread.sleep(500);
-        }
-        throw new TimeoutException("Element not clickable: " + element);
-    }
-
-    private void waitForUrlContains(String urlPart) throws InterruptedException {
-        for (int i = 0; i < 30; i++) { // Max 15 seconds (30 * 500ms)
-            if (driver.getCurrentUrl().contains(urlPart)) {
-                return;
-            }
-            Thread.sleep(500);
-        }
-        throw new TimeoutException("URL does not contain: " + urlPart);
     }
 }
